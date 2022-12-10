e9b9eef1ac5c3d2aef49537bf911dbe9ef3eb8be
Tkpointz <pointzztk@gmail.com>
2022-12-10 17:19:19 +0000

    Remove Kill Dialog from Kill button
    Change-Id: I45bd5366f66828b0b475b786979a4ef3baee917f
    Signed-off-by: Tkpointz <pointzztk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInfo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/NotificationInfo.java
@@ -376,21 +376,10 @@ public class NotificationInfo extends LinearLayout implements NotificationGuts.G
                         // Don't do anything
                         return;
                     }
-                    final SystemUIDialog killDialog = new SystemUIDialog(mContext);
-                    killDialog.setTitle(mContext.getText(R.string.force_stop_dlg_title));
-                    killDialog.setMessage(mContext.getText(R.string.force_stop_dlg_text));
-                    killDialog.setPositiveButton(
-                            R.string.dlg_ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // kill pkg
-                            ActivityManager actMan =
-                                    (ActivityManager) mContext.getSystemService(
-                                    Context.ACTIVITY_SERVICE);
-                            actMan.forceStopPackage(mPackageName);
-                        }
-                    });
-                    killDialog.setNegativeButton(R.string.dlg_cancel, null);
-                    killDialog.show();
+                    ActivityManager actMan =
+			(ActivityManager) mContext.getSystemService(
+	                Context.ACTIVITY_SERVICE);
+                    actMan.forceStopPackage(mPackageName);
                 }
             });
         } else {
